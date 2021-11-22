52e08b3067461018ae18fc29e4d36e9362541a63
Basil Aleksandrov <basilaleks@MacBook-Pro-Basil.local>
2021-11-22 09:42:34 +0300

    module-03 task-01 broken tests

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,11 +2,22 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
+  constructor(option) {
+    super(option);
+    this.limit = option.limit;
+    this.encoding = option.encoding;
+    this.fileSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
+
+      this.fileSize += chunk.length;
+
+      if (this.fileSize > this.limit) {
+         return callback(new LimitExceededError());
+      } else {
+        callback(null, chunk);
+      }
 
   }
 }
