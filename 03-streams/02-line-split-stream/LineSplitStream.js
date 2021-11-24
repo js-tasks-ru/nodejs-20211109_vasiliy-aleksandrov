e56382a4ad19b4a572254528747994e95acb7bb6
Basil Aleksandrov <basilaleks@MacBook-Pro-Basil.local>
2021-11-24 15:53:00 +0300

    solution module-03 task-02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.encoding = options.encoding; 
+    this.remainingData = ''
   }
 
   _transform(chunk, encoding, callback) {
+    let stringData = chunk.toString(this.encoding || 'utf-8');
+    let updatedData = this.remainingData + stringData;
+    this.remainingData = updatedData.slice(updatedData.lastIndexOf(os.EOL) + 1);
+    updatedData = updatedData.lastIndexOf(os.EOL) === -1 ? '' : updatedData.slice(0, updatedData.lastIndexOf(os.EOL));
+
+    updatedData.split(os.EOL).forEach((item) => {
+      this.push(item);
+    })
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.remainingData);
   }
 }
 
