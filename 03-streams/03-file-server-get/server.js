f816768033926f574529bcc0fc5ddb8ee9df39eb
Basil Aleksandrov <basilaleks@MacBook-Pro-Basil.local>
2021-11-26 15:24:58 +0300

    solution module-02 task-03

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,55 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+
+const mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+};
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
+  const ROOT = __dirname + '/files';
   const filepath = path.join(__dirname, 'files', pathname);
+  const type = mime[path.extname(filepath).slice(1)] || 'text/plain';
+  const stream = fs.createReadStream(filepath);
 
   switch (req.method) {
     case 'GET':
 
+      if (pathname.indexOf('/') !== -1 && pathname.indexOf('.') === -1) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end('No such file or directory');
+      }
+
+      if (filepath.indexOf(ROOT) !== 0) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Forbidden');
+      }
+
+      stream.on('open', () => {
+          res.setHeader('Content-Type', type);
+          stream.pipe(res);
+      });
+
+      stream.on('error', () => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Not found');
+      });
       break;
 
     default:
